e123ea68e0463600a81db560800226ee085a5047
Klalvarezm <68162132+Klalvarezm@users.noreply.github.com>
2021-12-13 18:48:05 -0500

    Added HomePage and Initial Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Home from './home';
 import Login from './login/login';
 import Register from './register/register';
 import GetUsers from './getUser/getUsers';
@@ -11,11 +12,31 @@ import GetCoursesAdmi from './courses/courseAdmi';
 import UpdateCourse from './update/updateCourse';
 import CourseTeacher from './courses/courseTeacher';
 import EditProjectTeacher from './update/updateCourseTeacher';
-
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem ,Container} from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
+    <Navbar bg="light" expand="lg">
+     <Container>
+    <Navbar.Brand href="/">EducaTEam</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/login">Inicia</Nav.Link>
+        <Nav.Link href="/register">Registrate</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     <Routes>
+    <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
       <Route path='/allUsers' element={<GetUsers/>}/>
