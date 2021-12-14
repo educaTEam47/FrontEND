65d3192bc64106e86ceedc773a239d7beecff964
Klalvarezm <68162132+Klalvarezm@users.noreply.github.com>
2021-12-13 21:13:00 -0500

    Update header.js

--- src/header.js
+++ src/header.js
@@ -12,9 +12,12 @@ function Header() {
             tokenStorage = ""
         }
     }, [])
-    const [auth, setAuth] = useState(false)
-    const [teacher,setTeacher] = useState(false)
-    const [student,setStudent] = useState(false)
+    const [auth, setAuth] = useState(false);
+    const [teacher,setTeacher] = useState(false);
+    const [student,setStudent] = useState(false);
+    const [admin,setAdmin] = useState(false);
+    const [name,setName]=useState("");
+    var [logs,setLogs]=useState("");
     useEffect(() => {
         response()
     }, [])
@@ -28,37 +31,44 @@ function Header() {
         console.log(response1)
         if (response1)
             if (response1.data.validate.rol !== "") {
+                console.log("Rol: "+response1.data.validate.rol);
+                setLogs(response1.data.validate.rol);
                 setAuth(response1.data.validate.validacion)
+                setName(response1.data.validate.nombres);
                 if(response1.data.validate.rol === "Lider"){
-                    setTeacher(response1.data.validate.rol)
+                    setTeacher(true);
                 }
                 else if(response1.data.validate.rol==="Estudiante"){
-                    setStudent(response1.data.validate.rol)
+                    setStudent(true);
+                } else if(response1.data.validate.rol==="Admi"){
+                    setAdmin(true);
                 }
             }
     }
 //################################################################################################################
 
-
+var isLogged=false;
+console.log("Muestra "+logs);
+if(!!logs){
+    isLogged=true;
+}
+console.log(name);
 return (
     <Navbar bg="light" expand="lg">
         <Container>
-            <Navbar.Brand href="/">EducaTEam</Navbar.Brand>
+            {!isLogged && <Navbar.Brand href="/">EducaTEam</Navbar.Brand>}
+            {isLogged &&<Navbar.Brand href="/profile">EducaTEam</Navbar.Brand>}
+            {isLogged && <Navbar.Text>Bienvenid@ {name}</Navbar.Text>}
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="/login">Inicia</Nav.Link>
-                    {teacher &&
-                        <Nav.Link href="/courseteacher">Mis cursos</Nav.Link>
-                    }
-                    <Nav.Link href="/register">Registrate</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                   {!isLogged && <Nav.Link href="/login">Inicia</Nav.Link>}
+                    {teacher && <Nav.Link href="/courseteacher">Mis cursos</Nav.Link>}
+                    {admin && <Nav.Link href="/allUsers">Todos los usuarios</Nav.Link>}
+                    {admin && <Nav.Link href="/getProjectsAdmi">Todos los Proyectos</Nav.Link>}
+                    {(admin || teacher)  && <Nav.Link href="/user">Busca un usuario</Nav.Link>}
+                    {!isLogged && <Nav.Link href="/register">Registrate</Nav.Link>}
+                   
                 </Nav>
             </Navbar.Collapse>
         </Container>
