0bb6729cdf819761e2cceae1629ba31ba9d9b629
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2022-12-18 15:15:49 -0500

    asd

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,41 @@
-import React from 'react';
-import './index.css';
-import App from './App';
+import React from "react";
+import "./index.css";
+import App from "./App";
 import { render } from "react-dom";
-import reportWebVitals from './reportWebVitals';
-import {ApolloClient, createHttpLink, InMemoryCache, ApolloProvider} from '@apollo/client'
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter} from "react-router-dom"
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter } from "react-router-dom";
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8000/',
+  uri: "http://localhost:8000/",
 });
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : "",
-    }
-  }
+    },
+  };
 });
 
 const client = new ApolloClient({
-  cache:new InMemoryCache(),
-  link: authLink.concat(httpLink)
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
-
-const rootElement = document.getElementById("root");
-render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  </ApolloProvider>,
-  rootElement
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
