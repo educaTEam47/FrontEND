dbfd858102861ceaae2a449fbf3215c7ce846079
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2022-12-18 15:18:16 -0500

    Revert "sd"
    This reverts commit 19eee113257d48aea76bfba9e9f7adb21977e363.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.css";
 import App from "./App";
-import ReactDOM from "react-dom/client";
 import { render } from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import {
